d177c15edaff5011bd31aace90424b4592dacdd0
Shad3 <122724277+LONELY-DUckWolf@users.noreply.github.com>
2025-05-12 23:25:06 +0300

    dwdw

---
+++ dist/dz6.81a0d1b8.js
@@ -0,0 +1,2 @@
+const u=Math.floor(100*Math.random())+1;console.log(`\u{421}\u{43B}\u{443}\u{447}\u{430}\u{439}\u{43D}\u{43E}\u{435} \u{447}\u{438}\u{441}\u{43B}\u{43E}: ${u}`);
+//# sourceMappingURL=dz6.81a0d1b8.js.map
